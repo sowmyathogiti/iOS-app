90b7ff5203cba2d3d5c58da3de5bce967114a3d6
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-02-16 14:51:54 -0600

    changes

--- work.js
+++ work.js
@@ -1,10 +1,11 @@
-function calculate(){
+function finalbill(){
     let totals = parseFloat(document.getElementById("total").value);
-    console.log(typeof total);
+    console.log(typeof totalIn);
     let rates = parseFloat(document.getElementById("rate").value);
     
-    document.getElementById("tip").innerHTML = "$" + rates*totals/100;
+    document.getElementById("tip").innerHTML = "$" + totals*rates/100;
     document.getElementById("tax").innerHTML = "$" + totals*0.055;
-    document.getElementById("grand").innerHTML = "$" + (totals + (rates*totals/100) + (totals*0.055));
+    document.getElementById("grand").innerHTML = "$" + (totals + (totals*rates/100) + (totals*0.055));
     return false;
+
 }
